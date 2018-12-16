f48bb15671ae1028f9f1018bcbb8d49a2ab9966b
Leonardo Bianco <leonardobianco@MBP-di-Leonardo.homenet.telecomitalia.it>
2018-12-16 15:08:54 +0100

    Creato un semplice test, e aggiunto file migrazione

--- libreria/tests.py
+++ libreria/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Autore, Genere, Libro
 
-# Create your tests here.
+# Semplice test per l'autore
+class AutoreTestCase(TestCase):
+    def setUp(self):
+        Autore.objects.create(nome="Dante", cognome="Alighieri", nazione="Italia")
+
+    def test_string_autore(self):
+        dante = Autore.objects.get(id=1)
+        self.assertEqual(dante.__str__(), "Dante Alighieri")
